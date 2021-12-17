0ff45e7e5934b1f07e2fc50c3f2c0587cd209dae
KartikChippalkati <karucmc@gmail.com>
2021-12-18 02:50:15 +0530

    Dice game

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,63 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
+        backgroundColor: Colors.black,
+        appBar: AppBar(title: Text('Dicee'), backgroundColor: Colors.red),
         body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  //const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber =
+      1; // rightDiceNumber = 0; , this should not be done bcz at first running or first cycle the variable take the value 0(zero) and the image name Dice0 does't exist and when u hot reload it will first show "no image or image not present" and once u pressed then it will show image
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  //print("hello Baby");
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image(
+                image: AssetImage('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  // print("hello Kartik");
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+                // print("hello Kartik");
+                rightDiceNumber = Random().nextInt(6) + 1;
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
